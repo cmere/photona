18b58370c0b38002f086fff98c18cc0259947484
Haiyong Xu <haiyeong@gmail.com>
2017-02-07 00:14:32 -0500

    test multiple clients connection

--- src/SocketServer_test/main.cpp
+++ src/SocketServer_test/main.cpp
@@ -6,9 +6,10 @@
 #include <unistd.h>
 
 #include "include/first.hpp"
-#include "SocketServer/TCPSocket.hpp"
+#include "SocketServer/FDSelector.hpp"
 #include "SocketServer/MessageBuffer.hpp"
 #include "SocketServer/MessageTest.hpp"
+#include "SocketServer/TCPSocket.hpp"
 
 using namespace std;
 using namespace SocketServer;
@@ -23,21 +24,45 @@ int main(int argc, char *argv[])
   string serverIPAddress = argv[1];
   unsigned int serverPort = stoi(argv[2]);
 
-  TCPSocket tcpSocket;
-  cout << "connecting to " << serverIPAddress << ":" << serverPort << endl;
-  if (!tcpSocket.connectTo(serverIPAddress, serverPort)) {
-    cout << "socket connect failed: " << strerror(errno) << endl;
-    return EXIT_FAILURE;
+  constexpr unsigned int N = 5;
+  shared_ptr<TCPSocket> pSockets[N];
+  FDSelector selector;
+
+  for (unsigned int i = 0; i < N; ++i) {
+    pSockets[i].reset(new TCPSocket());
+    cout << "connecting to " << serverIPAddress << ":" << serverPort << endl;
+    if (!pSockets[i]->connectTo(serverIPAddress, serverPort)) {
+      cout << "socket connect failed: " << strerror(errno) << endl;
+      return EXIT_FAILURE;
+    }
+    cout << "fd=" << pSockets[i]->fd() << " connected " << pSockets[i]->getLocalPair() << " ->  " <<  pSockets[i]->getPeerPair() << endl;
+    auto pMsg = make_shared<MessageTest>(pSockets[i]->getClientID());
+    pMsg->setBody("hello " + to_string(i));
+    MessageBuffer::Singleton().queueMessageToSend(pMsg);
+    selector.addToReadSelectable(pSockets[i]);
+    selector.addToWriteSelectable(pSockets[i]);
   }
 
-  cout << "fd=" << tcpSocket.fd() << " connected to " << serverIPAddress << ":" << serverPort << endl;
+  while (1) {
+    int sel = selector.select();
+    cout << "select() = " << sel << endl;
+    if (sel <= 0) {
+      return EXIT_FAILURE;
+    }
 
-  string line;
-  while (getline(cin, line)) {
-    auto pMsg = make_shared<MessageTest>(tcpSocket.getLocalPair(), tcpSocket.getPeerPair());
-    pMsg->setBody(line);
-    MessageBuffer::Singleton().queueMessageToSend(pMsg);
-    cout << "sent " << tcpSocket.handleSelectWritable() << " bytes." << endl;
+    auto pReadSockets = selector.getReadyToRead();
+    cout << "socket ready to read " << pReadSockets.size() << endl;
+    auto pWriteSockets = selector.getReadyToWrite();
+    cout << "socket ready to write " << pWriteSockets.size() << endl;
+
+    for (auto pReadSocket : pReadSockets) {
+      if (pReadSocket->handleSelectReadable() <= 0) {  // eof or error
+        pWriteSockets.erase(pReadSocket);
+      }
+    }
+    for (auto pWriteSocket : pWriteSockets) {
+      pWriteSocket->handleSelectWritable();
+    }
   }
 
   return EXIT_SUCCESS;
