ce8697e7bb7332274ba785a2f943986854c46fad
Haiyong Xu <haiyeong@gmail.com>
2017-03-07 00:10:54 -0500

    test parsing errors

--- src/SocketServer_test/main.cpp
+++ src/SocketServer_test/main.cpp
@@ -10,6 +10,7 @@
 #include "SocketServer/Logger.hpp"
 #include "SocketServer/MessageBuffer.hpp"
 #include "SocketServer/MessageEcho.hpp"
+#include "SocketServer/MessageTest.hpp"
 #include "SocketServer/TCPSocket.hpp"
 
 using namespace std;
@@ -31,6 +32,7 @@ int main(int argc, char *argv[])
   shared_ptr<TCPSocket> pSockets[N];
   FDSelector selector;
 
+  // create N socket, send one MessageEcho on each socket.
   for (unsigned int i = 0; i < N; ++i) {
     pSockets[i].reset(new TCPSocket());
     if (!pSockets[i]->connectTo(serverIPAddress, serverPort)) {
@@ -45,8 +47,40 @@ int main(int argc, char *argv[])
     selector.addToWriteSelectable(pSockets[i]);
   }
 
+  // send MessageTest 
+  {
+    auto pTestSocket = make_shared<TCPSocket>();
+    if (!pTestSocket->connectTo(serverIPAddress, serverPort)) {
+      logger << " socket connect failed: " << strerror(errno) << endlog;
+      return EXIT_FAILURE;
+    }
+    logger << logger.test << "fd=" << pTestSocket->fd() << " connected " << pTestSocket->getLocalPair() << " ->  " <<  pTestSocket->getPeerPair() << endlog;
+    selector.addToReadSelectable(pTestSocket);
+    selector.addToWriteSelectable(pTestSocket);
+
+    // send message with unknow type, server should ignore msg.
+    auto pMsg = make_shared<MessageTest>();
+    pMsg->setData("Test unknown message type");
+    pMsg->setMessageType(-1);
+    MessageBuffer::Singleton().queueMessageToSend(pMsg, pTestSocket->getSocketID());
+
+    // send message with truncated data, server should ignore data.
+    pMsg = make_shared<MessageTest>();
+    pMsg->setSendTruncatedData();
+    MessageBuffer::Singleton().queueMessageToSend(pMsg, pTestSocket->getSocketID());
+
+    // send normal data, server should receive data.
+    pMsg = make_shared<MessageTest>();
+    pMsg->setData("0123456789");
+    MessageBuffer::Singleton().queueMessageToSend(pMsg, pTestSocket->getSocketID());
+  }
+
+  timeval timeout;
+  timeout.tv_sec = 1;
+  timeout.tv_usec = 0;
+
   while (1) {
-    int sel = selector.select();
+    int sel = selector.select(&timeout);
     logger << logger.debug << "select() = " << sel << endlog;
     if (sel <= 0) {
       logger << "select return 0, exit" << endlog;
@@ -66,7 +100,6 @@ int main(int argc, char *argv[])
     for (auto pWriteSocket : pWriteSockets) {
       pWriteSocket->handleSelectWritable();
     }
-    break;
   }
 
   logger.closeLog();
